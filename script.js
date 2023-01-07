ea18ac7f6dbb7b923579ab6e0c9cae953b96e9cf
S4NGAL <41579632+S4NGAL@users.noreply.github.com>
2023-01-07 12:26:26 +0300

    add display function

--- script.js
+++ script.js
@@ -1,36 +1,43 @@
 let numbers = ["0", "1", "2","3", "4", "5", "6", "7", "8", "9"]
 let signs = ["+", "-", "/", "X"]
-
+let text;
+const display = document.getElementById("display");
 let answer = 0;
 let equation1 = "";
 let equation2 = "";
 let total = 0;
 let singHolder = "";
+display.textContent = equation1+ singHolder + equation2;
 
 const equal = document.getElementById("equals");
 equal.addEventListener("click", function(){
     switch (singHolder) {
         case "+": 
             answer = Number(equation1) + Number(equation2);
-            console.log(equation1 + " + " + equation2 + "= ", Number(equation1) + Number(equation2));
+            text = equation1 + " + " + equation2 + " = " + String(Number(equation1) + Number(equation2));
+            console.log(text);
+            display.textContent = text;
             reset();
             break;
         
         case "-":   
-            answer = Number(equation1) + Number(equation2);
-            console.log(equation1 + " - " + equation2 + "= ", Number(equation1) - Number(equation2));
+            answer = Number(equation1) - Number(equation2);
+            text = equation1 + " - " + equation2 + " = " + String(Number(equation1) - Number(equation2));
+            display.textContent = text;
             reset();
             break;
         
         case "/":   
             answer = Number(equation1) / Number(equation2);
-            console.log(equation1 + " / " + equation2 + "= ", Number(equation1) / Number(equation2));
+            text = equation1 + " / " + equation2 + " = " + String(Number(equation1) / Number(equation2));
+            display.textContent = text;
             reset();
             break;
         
         case "X":   
-            console.log(equation1 + " X " + equation2 + "= ", Number(equation1) * Number(equation2));
+            text = equation1 + " X " + equation2 + " = " + String(Number(equation1) * Number(equation2));
             answer = Number(equation1) * Number(equation2);
+            display.textContent = text;
             reset();
             break;
     }
@@ -50,13 +57,19 @@ function calculate(var1) {
         if (singHolder === ""){
             equation1 += var1;
             console.log(equation1)
+            display.textContent = equation1+ singHolder + equation2;
+
         } else {
             equation2 += var1;
             console.log(equation2);
+            display.textContent = equation1+ singHolder + equation2;
+
         }
     } else if(signs.includes(var1)){
         singHolder = var1;
         console.log(singHolder);
+        display.textContent = equation1+ singHolder + equation2;
+
     }
 }
 
@@ -76,10 +89,18 @@ answerButton.addEventListener("click", function(){
     if (equation1 === "") {
         console.log(answer)
         equation1 = answer;
+        display.textContent = equation1+ singHolder + equation2;
     }else if (equation2 === ""){
         console.log(answer)
         equation2 = answer;
+        display.textContent = equation1+ singHolder + equation2;
+
     }else{
         console.log("!!!both equations are fullified!!!!");
     }
 });
+
+
+function displayUpdater(){
+    display.textContent = equation1+ singHolder + equation2;
+}
